c5c550dfc91b4cf83ca5f7a29ee71eef0b8e1670
Fred Sigworth <fred@Mini2.local>
2020-11-01 17:20:34 -0500

    MakeFilteredJpegsFromMrcs mod for old ztiff files

--- MultiExposure/MakeFilteredJpegsFromMrcs.m
+++ MultiExposure/MakeFilteredJpegsFromMrcs.m
@@ -1,13 +1,16 @@
 % MakeFilteredJpegsFromMrcs
 % Read mi files from the Info directory. Based on that, load a merged image
-% (preferably *s.mrc), inverse-CTF filter it, and save it as a jpeg in the 
+% (preferably *s.mrc), inverse-CTF filter it, and save it as a jpeg in the
 % jpegPath directory.
 figure(1);
 clf;
 disp(['Converting mrc images to jpgs in base directory ' pwd]);
-allNames=f2FindInfoFiles;
-targetSize=960;
-% targetSize=1024;
+
+infoDir='Info';
+
+allNames=f2FindInfoFiles(infoDir);
+targetSize=768;
+targetSize=1024;
 % suffix={'s' 'vs'};
 suffix={'s'};
 readZTiff=0;
@@ -18,21 +21,24 @@ doWrite=1;
 invCTF=0.3;
 finalLowpass=0.2;
 
-maxImages=inf;
+maxImages=200;
+iStart=1;
 
 CheckAndMakeDir(jpegPath,1);
 
-ni=min(numel(allNames),maxImages);
+ni=min(numel(allNames)-iStart+1,maxImages);
 disp(['Working on ' num2str(ni) ' images.']);
-for i=1:ni
+
+
+for i=iStart:iStart-1+ni
     mi=ReadMiFile(allNames{i});
     for j=1:numel(suffix) % read each row of the suffix array
-if readZTiff
-    [m,pixA,ok]=ReadEMFile(['Merged/' mi.baseFilename 'mz.tif']);
-else
+        if readZTiff
+            [m,pixA,ok]=ReadEMFile(['Merged/' mi.baseFilename 'mz.tif']);
+        else
             [m, mergeFullName,ok]=meReadMergedImage(mi,0,suffix{j});
-end;
-    if ~ok
+        end;
+        if ~ok
             continue;
         end;
         imSize=size(m);
@@ -41,23 +47,23 @@ end;
         ds=round(ds0);
         if abs(1-ds/ds0)>.2 % allowed tolerance in downsampling ratio
             disp(['Downsampling ratio discrepancy: ' num2str([ds0 ds]) '   ' num2str([n targetSize])]);
-                return
+            return
         end;
         md=Downsample(m,round(size(m)/ds));
         mdf=GaussFilt(md,finalLowpass);
         if invCTF
-           mdf=meCTFInverseFilter(mdf,mi,invCTF);
+            mdf=meCTFInverseFilter(mdf,mi,invCTF);
         end;
         defStr=[num2str(mi.ctf(1).defocus,3) 'um'];
         invStr=[num2str(invCTF,2) 'inv'];
         extraString=['  ' defStr ' ' invStr];
-if readZTiff
-    baseName=mi.baseFilename;
-else
-     [pa,baseName,ex]=fileparts(mergeFullName);    
-end;
-
-    jpegName=[jpegPath baseName '_' defStr '_' invStr '.jpg'];
+        if readZTiff
+            baseName=mi.baseFilename;
+        else
+            [pa,baseName,ex]=fileparts(mergeFullName);
+        end;
+        
+        jpegName=[jpegPath baseName '_' defStr '_' invStr '.jpg'];
         msc=WriteJpeg(mdf,jpegName,clipThreshold,doWrite);
         imaga(msc);
         title([num2str(i) '  ' jpegName],'interpreter','none');
