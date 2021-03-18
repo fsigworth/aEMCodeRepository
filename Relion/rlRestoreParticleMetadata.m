692758fef867d78594993154dd4662fb008f83f8
Fred Sigworth <fred.sigworth@yale.edu>
2021-03-18 13:35:59 -0400

    rlRestoreParticleMetadata

--- Relion/rlRestoreParticleMetadata.m
+++ Relion/rlRestoreParticleMetadata.m
@@ -8,29 +8,41 @@
 % and coordinates) and place this into C. 
 
 % The fields to be added or replaced are specified here:
-theFields={'rlnOpticsGroup'
-           'rlnGroupName'
-           'rlnGroupNumber'};
+% theFields={'rlnOpticsGroup'
+%            'rlnGroupName'
+%            'rlnGroupNumber'};
+theFields={'vesicleRadius' ; 'vesiclePsi'};
 
-starAName='RSC/particleAll9_intens+frac_7505.star';
-starBName='Select/job236/particles.star';
-starCName='Select/job236/particles_new_metadata.star';
+% starAName='RSC/particleAll9_intens+frac_7505.star';
+starAName='Class2D/job235/run_ct15_it020_data.star'
+starBName='Class3D/job245/run_ct10_it025_data_original.star'
+starCName='Class3D/job245/run_ct10_it025_data.star'
 
+if ~exist(starAName,'file'), disp([starAName ' not found.']); end;
+if ~exist(starBName,'file'), disp([starBName ' not found.']); end;
+if ~exist(starCName,'file'), disp([starCName ' not found.']); end;
 %%
 disp(['Reading ' starAName]);
 [nmA,aDat]=ReadStarFile(starAName);
 aPts=aDat{2};
 aNp=numel(aPts.rlnMicrographName);
 %%
-    disp(['Reading ' starBName]);
-        [nmB,bDat]=ReadStarFile(starBName);
-        bPts=bDat{2};
-        nbLines=numel(bPts.rlMicrographName);
-%% Copy B into C
+for i=1:numel(theFields)
+    if ~isfield(aPts,theFields{i})
+        disp(['This field is not found: ' theFields{i}]);
+        return
+    end;
+end;
+%%
+disp(['Reading ' starBName]);
+[nmB,bDat]=ReadStarFile(starBName);
+bPts=bDat{2};
+nbLines=numel(bPts.rlnMicrographName);
+% Copy B into C
 nmC=nmB;
 cDat=bDat;
 cPts=cDat{2};
-    %% Copy the optics info from A in t C
+    % Copy the optics info from A in t C
     cDat{1}=aDat{1};
 
     %% get the micrograph names from the subset file B
@@ -38,24 +50,24 @@ cPts=cDat{2};
 bNumNames=numel(bNames);
 disp([num2str(bNumNames) ' unique micrographs']);
 
-%% Get the full-set A micrograph names.
+% Get the full-set A micrograph names.
 [aNames,~,aPartInds]=unique(aPts.rlnMicrographName);
 disp([num2str(numel(aNames)) ' A micrograph refs']);
 
-%% Get iaLines, the A line numbers corresponding to each B line.
+% Get iaLines, the A line numbers corresponding to each B line.
 iaLines=zeros(bNumNames,1);
 for i=1:bNumNames % look at each B micrograph name
     % find all the particles having that name
     ibParts=find(bPartInds==i);
-    bXs=pts.rlnCoordinateX(ibParts);
-    bYs=pts.rlnCoordinateY(ibParts);
+    bXs=bPts.rlnCoordinateX(ibParts);
+    bYs=bPts.rlnCoordinateY(ibParts);
     
     % find the correspoinding A name index
     j=find(strcmp(bNames{i},aNames),1);
     iaParts=find(aPartInds==j); % all the A particles with that name
     % now we find which coordinates match
-    aXs=ves.ptlX(iaParts);
-    aYs=ves.ptlY(iaParts);
+    aXs=aPts.rlnCoordinateX(iaParts);
+    aYs=aPts.rlnCoordinateY(iaParts);
     dists=hypot(bXs-aXs',bYs-aYs');
     [minDists,iaLocal]=min(dists,[],2);
     iaLines(ibParts)=iaParts(iaLocal);
@@ -73,5 +85,6 @@ end;
 cDat{2}=cPts;
 
 % Write out the C star file.
+disp(['Writing ' starCName]);
 hdrText=['# version 30001; using metadata from ' starAName];
 WriteStarFile(nmC,cDat,starCName,hdrText);
