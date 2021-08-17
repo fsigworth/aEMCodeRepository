653fc141a51c12a3218059f5c92f4d605971b21c
Fred Sigworth <fred.sigworth@yale.edu>
2021-08-16 20:51:31 -0400

    startup.m

---
+++ startup.m
@@ -0,0 +1,44 @@
+% startup.m
+% Put this script somewhere where Matlab will look on startup, e.g. in home
+% folder or in ~/Documents/MATLAB
+host='MyHost';
+homePath='~/';
+basePath='~/aEMCodeRepository/'; % We asssume that the code is here.
+
+disp([which('startup') ' on ' host]);
+
+setenv('HOSTNAME',host);
+cd(homePath);
+folders={['aLibs' filesep 'EMBase']
+         ['aLibs' filesep 'EMCCD']
+         ['aLibs' filesep 'EMIO']
+         ['aLibs' filesep 'EMIOUtils']
+         ['aLibs' filesep 'EMSpec']
+         ['aLibs' filesep 'GriddingLib']
+         ['aLibs' filesep 'MEX'   ]   
+         ['aLibs' filesep 'Others']
+         'EMClass'
+         'FourierReconstruction'
+         'K2Camera'
+         'MultiExposure'
+         'Relion'
+         'Realtime'
+         'RSC'
+         'RSC-Apps'
+         'RSC-utilities'
+         'RSCPicking'
+         'RSCReconstruct'
+         'RSCSimulate'
+         'RSCVesicleFinder'
+         'RSCAnalysis'
+         'AMPAR'
+         'Kv'
+%         ['aLibs' filesep 'Others' filesep 'bfmatlab']
+%          'RSCAdaptiveBasis'
+%          'RSCAdaptiveBasis/utils'
+         };
+         
+for i=1:numel(folders)
+    addpath([basePath folders{i}]);
+end;
+
